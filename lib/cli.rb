432a0522cdec956f9a1d2babdd3f7d6d8775ce78
Zoe Williams <mwilliamszoe@gmail.com>
2018-09-14 09:39:51 -0500

    Add exit option

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,9 +4,9 @@ class Cli
 
   @@critic = nil
 
-  def greet
-    puts "Welcome to The Movie Reviewer! Here are all our movies."
-  end
+  # def greet
+  #   puts "Welcome to The Movie Reviewer! Type in a movie from the list to see more options."
+  # end
 
   # def main_menu
   #   puts "What would you like to do? See our m"
@@ -26,7 +26,7 @@ class Cli
   end
 
   def movie_choice
-    puts "Select a movie from the list."
+    puts "Welcome to Movie Town! Select a movie from the list."
     list_movies
     movie_input = gets.chomp
     movie_instance = Movie.find_by(title: movie_input)
@@ -39,11 +39,11 @@ class Cli
   end
 
   def movie_options(movie_obj)
-    puts "Type in the number of your selected option"
+    puts "Type in the number of your selected option or press 4 to exit the app."
     puts "1. See movie reviews."
     puts "2. Write a review for this movie"
     puts "3. Pick a new movie"
-    
+    puts "4. Exit the app"
     n = gets.chomp
    case n
      when 1.to_s
@@ -52,21 +52,28 @@ class Cli
       create_review(movie_obj)
      when 3.to_s
        movie_choice
+     when 4.to_s
+      exit!
      else
       "Sorry not valid input. Please make another selection."
       movie_options(movie_obj)
    end
   end
 
-  def show_movie_reviews(users_movie_obj)
+  def display_reviews(users_movie_obj)
     reviews_arr = users_movie_obj.reviews
     if reviews_arr.empty?
-      puts "its empty"
+      puts "There are no reviews for this movie."
     else
       reviews_arr.each do |review|
-        puts "#{review.id}. #{review.critic.name} said '#{review.phrase}' about #{review.movie.title}."
+        puts "#{review.id}. #{review.critic.name} said '#{review.phrase}'."
+        # puts "#{review.id}. #{review.critic.name} said '#{review.phrase}' about #{review.movie.title}."
       end
     end
+  end
+
+  def show_movie_reviews(users_movie_obj)
+    display_reviews(users_movie_obj)
     review_options(users_movie_obj)
   end
 
@@ -75,6 +82,8 @@ class Cli
     puts "1. Edit a review"
     puts "2. Delete a review"
     puts "3. Create a new review"
+    puts "4. Pick a new movie"
+    puts "5. Exit the app"
     response = gets.chomp
     case response
     when "1"
@@ -83,16 +92,22 @@ class Cli
       change_a_review(id)
     when "2"
       puts "Which review? You can simply enter the number next to your chosen review."
+      # puts "#{Review.all.id}.#{Review.all}."
+      display_reviews(movie_instance)
       id = gets.chomp.to_i
       delete_a_review(id)
     when "3"
       create_review(movie_instance)
+    when "4"
+      movie_choice
+    when "5"
+      exit!
     else
       "Sorry, that choice isn't valid. Please try again."
       review_options
     end
+    movie_choice
   end
-
 
   def create_review(movie_instance)
     puts "Type in your name please"
@@ -100,10 +115,11 @@ class Cli
     puts "type your review"
     user_review = gets.chomp
     current_critic = Critic.find_or_create_by(name: user_name)
-    your_review = Review.create(critic_id: current_critic, movie_id: movie_instance, phrase: user_review)
-    puts your_review
+    your_review = Review.create(critic_id: current_critic.id, movie_id: movie_instance.id, phrase: user_review)
+    binding.pry
+    puts "Great! Your review '#{your_review.phrase}' has been created."
+    movie_choice
   end
-
 
   def change_a_review(review_id)
     updatable_review = Review.find_by(id: review_id)
@@ -140,8 +156,7 @@ class Cli
   end
 
   def run
-    greet
-    log_in
+    # greet
     movie_choice
   end
 
